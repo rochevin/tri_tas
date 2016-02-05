2c0d92b27b2b0a773198f1e2c0aec95c83ea599e
rochevin <vincent.rocher@etu.univ-rouen.fr>
2016-02-05 15:30:08 +0100

    Ajout de getopt dans main

--- main.c
+++ main.c
@@ -1,70 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "triTas.h"
 #include <time.h>
+#include <unistd.h>
+#include <getopt.h>
+#include "triTas.h"
+#include "fichier.h"
+
+//Fonction qui affiche la bonne utilisation du programme
+void prog_usage(char* progname){
+	fprintf(stderr, "Usage: %s [-r] [-i input file name (non optional)] [-o output file name (optional)]\n", progname) ;
+	exit(EXIT_FAILURE) ;
+}
 
 int main(int argc, char** argv) {
 	
-	//Si on a pas au moins le nom du programme, un nom de fichier en argument et un nom de fichier de sortie, on quitte le programme
-	if(argc < 3){
-		fprintf(stderr, "Veuillez préciser un nom de fichier en argument\n") ;
-		exit(EXIT_FAILURE) ;
+	//Definition des variables pour getopt
+	int option ;
+	char *input_file = NULL, *output_file = "output" ;
+	Pfonction Tamiser = Tamiser_max; //Définit un pointeur de fonction pour le choix recursif ou non, par défaut non recursif
+	//Parsing de getopt
+	while ((option = getopt(argc , argv, "i:o:r")) != -1){
+    	switch (option) {
+			case 'i':
+				input_file = optarg ; //On récupère le nom du fichier qui contient la liste d'entiers
+				break ;
+			case 'o':
+				output_file = optarg ; //On récupère le nom du fichier de sortie
+				break ;
+			case 'r':
+				printf("MESSAGE : Utilisation recursive du programme\n") ;
+				Tamiser = Tamiser_max_rec ; //dans le cas ou l'utilisateur selectionne -r, on utilise la fonction recursive
+				break ;
+			case '?':
+				prog_usage(argv[0]) ; //On print l'utilisation du programme dans le cas ou un argument inconnu est utilisé
+				break ;
+    	}
 	}
+
+	//On print l'utilisation du programme si input_file vaut NULL
+	if(input_file == NULL){
+		prog_usage(argv[0]) ;
+	}
+
 
 	//Partie qui va servir à calculer le temps d'execution des fonctions
 	float temps ;
 	clock_t t1,t2 ;
 
 	//On ouvre le fichier portant le nom donné en argument
-	FILE* fichier = OuvrirFichier(argv[1]) ;
+	FILE* fichier = OuvrirFichier(input_file) ;
 
 	//On détermine son nombre d'éléments
 	int nbElemts = GetNombreElements(fichier) ;
+
 	//Puis on construit le tableau à partir des entiers contenus dans le fichier 
 	int* Tas = ConstruireTableau(fichier,nbElemts) ;
+
 	//On ferme le fichier
 	fclose(fichier) ;
 
-	//Puisqu'on modifie directement le tableau, on le duplique pour l'appel non recursif
-	//Tas utilisé pour l'appel non recursif de la fonction
-	int* Tas_non_rec = Tas ;
-
 	//On affiche le nombre d'élements, puis le tableau
-	printf("%d éléments\n",nbElemts) ;
-	//AfficherTableau(Tas,nbElemts) ;
+	printf("MESSAGE : %d éléments à traiter\n",nbElemts) ;
 
-	//Partie appel recursif
+
+
 	t1 = clock();
 
-	ConstruireTas(Tas,nbElemts,Tamiser_max_rec) ;
-	//AfficherTableau(Tas,nbElemts) ;
-	TriTas(Tas,nbElemts,Tamiser_max_rec) ;
+	ConstruireTas(Tas,nbElemts,Tamiser) ;
+
+	TriTas(Tas,nbElemts,Tamiser) ;
 
 	t2 = clock();
 
-	//AfficherTableau(Tas,nbElemts) ;
-	EcrireFichier(argv[2],Tas,nbElemts) ;
+	EcrireFichier(output_file,Tas,nbElemts) ;
+	printf("MESSAGE : Tri enregistré dans \"%s\"\n",output_file) ;
 
 	temps = (float)(t2-t1)/CLOCKS_PER_SEC;
-	printf("temps = %f\n", temps);
-
-	//On libère l'espace mémoire alloué pour le Tas trié puisqu'il ne sert plus
-	free(Tas) ;
-
-	//Partie appel non recursif
-	t1 = clock();
-
-	ConstruireTas(Tas_non_rec,nbElemts,Tamiser_max) ;
-	//AfficherTableau(Tas_non_rec,nbElemts) ;
-	TriTas(Tas_non_rec,nbElemts,Tamiser_max) ;
-
-	t2 = clock();
-
-	//AfficherTableau(Tas_non_rec,nbElemts) ;
-
-	temps = (float)(t2-t1)/CLOCKS_PER_SEC;
-	printf("temps = %f\n", temps);
-
+	printf("MESSAGE : Temps d'éxecution = %f\n", temps);
 
 	return EXIT_SUCCESS;
 }
